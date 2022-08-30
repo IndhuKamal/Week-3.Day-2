67516bf38594bd4924db7e9fad4f6bd4f1ccfbaa
IndhuKamal <107314957+IndhuKamal@users.noreply.github.com>
2022-08-30 17:30:36 +0800

    Add files via upload

---
+++ MaxDuplicate.java
@@ -0,0 +1,33 @@
+package week3.Day2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxDuplicate {
+
+	public static void main(String[] args) {
+		String a = "abbaba";
+		Map<Character,Integer> hm= new HashMap<Character, Integer>();
+		char[] charArray = a.toCharArray();
+		int maxCount = 1;
+		char maxElement = charArray[0];
+		for(int i = 0;i<charArray.length;i++) {
+			if(hm.containsKey(charArray[i])) {
+				hm.put(charArray[i], hm.get(charArray[i])+1);
+			}
+			else {
+				hm.put(charArray[i], 1);
+			}		
+		
+		int count = hm.get(charArray[i]);
+		if(count>maxCount) {
+			maxCount = count;
+			maxElement = charArray[i];
+			}
+		}
+		System.out.println(maxElement);
+	}
+
+	}
+
+
